674127b60a02d44b6c909ba024d50abe3a5c9636
rgen4513 <70700028+rgen4513@users.noreply.github.com>
2021-06-10 23:50:48 +1000

    timer

--- public/script.js
+++ public/script.js
@@ -7,6 +7,10 @@ var estimatedTimeInput = document.getElementById("estimatedTimeInput");
 var priorityInput = document.getElementById("priorityInput");
 var tasklist = document.getElementById("tasklist");
 
+var covey_button = document.getElementById("covey_button");
+var pomodoro_button = document.getElementById("pomodoro_button");
+var dictionary_button = document.getElementById("dictionary_button");
+
 button.addEventListener("click", function(event){
   event.preventDefault();
   let taskIn = taskInput.value;
@@ -14,12 +18,17 @@ button.addEventListener("click", function(event){
   let completionInput = completionTimeInput.value;
   let estimatedTime = estimatedTimeInput.value;
   let priorityRating = priorityInput.options[priorityInput.selectedIndex].value;
-  addTask(taskIn,dueDate,priorityRating,completionInput,estimatedTime,"half")
-  console.log(taskList)
-})
+  addTask(taskIn,dueDate,priorityRating,completionInput,estimatedTime,"half");
+  //console.log(dueDate);
+  if (taskIn.length == 0 || dueDate.length == 0) {
+    alert("Your Task Is Not Filled In")
+  }
+});
 
+//Declare tasklist array to store the task info
 var taskList = [];
 
+// Create function to update taskList array
 function addTask(nam,due,prior,dueTime,estdtime,stat) {
   let task = {
    name:nam,
@@ -33,8 +42,9 @@ function addTask(nam,due,prior,dueTime,estdtime,stat) {
  renderTask(task);  
 }
 
+//Create function to update the DOM
 function renderTask(task){
-  //Create elements
+//Create elements
  let item = document.createElement("li");
  //item.innerHTML = "<div class=testDiv>" "</div>";
  item.innerHTML = "<div class=testDiv>" + "<p>" + task.name + "<br>" + "Is due on the" + task.duedate + "<br>" + task.priority + "<br>" + task.dueTime + "<br>" + task.estimatedtime + "<br>" + task.status + "</p>" + "</div>";
@@ -52,7 +62,62 @@ function renderTask(task){
   event.preventDefault();
   item.remove();
 })
-  //clear the input form
+  //clear input form
 form.reset();
 }
 
+/* Stopwatch Timer */
+var hr = 0, min = 0, sec = 0;
+var timer;
+
+var stopWatch = document.getElementById("stopwatch");
+var startButton = document.getElementById("Start");
+var stopButton = document.getElementById("Stop");
+var resetButton = document.getElementById("Reset");
+
+/* Listener for start button */
+startButton.addEventListener("click", start);
+
+/* Create function to run update second */
+function start() {
+  if (!timer) {
+    timer = setInterval(run,1000);
+  }
+}
+
+/* Create function to display the elapsed time */
+function run() {
+  stopWatch.innerHTML = (hr < 10 ? "0" + hr : hr) + ":" + (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec);
+  sec++;
+  /* Create conditionals to */
+  if(sec == 60) {
+    sec = 0;
+    min++;
+  } 
+  if(min == 60) {
+    min = 0;
+    hr++;
+  }
+}
+
+/* Listener for stop button */
+stopButton.addEventListener("click", stop);
+/* Create function to stop timer*/
+function stop() {
+  clearInterval(timer);
+  timer = false;
+}
+
+/* Listener for reset button */
+resetButton.addEventListener("click", reset);
+/* Create function to reset values*/
+function reset() {
+  clearInterval(timer);
+  timer = false;
+
+  sec = 0;
+  min = 0;
+  hr = 0;
+
+  stopWatch.innerHTML = (hr < 10 ? "0" + hr : hr) + ":" + (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec);
+}
